003f09b2240e9bca21d0aba5aa9a98c63c601fa5
5t4r-h4rf4 <bp0250@gmail.com>
2021-12-03 10:27:36 +0700

    Lazy Load

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -15,7 +15,7 @@ class BookController extends Controller
     public function index()
     {
         return view('book.index', [
-            'books' => Book::paginate(10)
+            'books' => Book::with('categories')->paginate(10)
         ]);
     }
 
